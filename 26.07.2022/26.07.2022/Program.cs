ee32e6ec0fc1de6fb61ef2e73d3ba5335bb64b9a
MaverickGODSENT <galinivaov88@gmail.com>
2022-07-31 23:57:12 +0300

    /

---
+++ 26.07.2022/26.07.2022/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static int smallest(int x, int y, int z)
+        {
+            int c = 0;
+
+            while (x != 0 && y != 0 && z != 0)
+            {
+                x--;
+                y--;
+                z--;
+                c++;
+            }
+
+            return c;
+        }
+
+        // Driver Code
+        public static void Main()
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            Console.Write("Minimum of 3"
+                          + " numbers is " + smallest(x, y, z));
+        }
+    }
+}
---
+++ 26.07.2022/Add and Subtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_and_Subtract
+{
+    internal class Program
+    {
+        static int Summing(int a, int b)
+        {
+            int c = a + b;
+        }
+        static int Subtract(int c, int x)
+        {
+             int f = c - x;
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            int c = Summing(a, b);
+            Console.WriteLine(c, Subtract(c, x));
+        }
+    }
+}
